d4573abc5b5797026f0bdedd775e6470128bb53a
Huy Nguyen <hnguyen10082000@gmail.com>
2024-01-16 13:48:56 -0500

    added routing and links to main page

--- index.php
+++ index.php
@@ -17,7 +17,11 @@ $f3 = Base::instance(); //static method
 
 // Define a default route
 $f3->route('GET /', function () {
-    echo "<h1> Test Test</h1>";
+    //echo "<h1> Test Test</h1>";
+
+    // Display a view page
+    $view = new Template();
+    echo $view->render('views/home.html');
 });
 //Run Fat-Free
 
