6b301789168a65a2a188af808787f2ddc07148e5
Huy Nguyen <hnguyen10082000@gmail.com>
2024-01-16 12:56:35 -0500

    added controller

--- index.php
+++ index.php
@@ -1,2 +1,29 @@
 <?php
-echo "<h1> Test Test</h1>";
+/*
+ * Huy Nguyen
+ * January 2024
+ * https://hnguyen.greenriverdev.com/328/diner/
+ * This is my CONTROLLER for the Diner app (Airport security)
+*/
+
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+// Require the autoload file
+require_once ('vendor/autoload.php');
+
+// Instantiate Fat-Free framework (F3)
+$f3 = Base::instance(); //static method
+
+// Define a default route
+$f3->route('GET /', function () {
+    echo "<h1> Test Test</h1>";
+});
+//Run Fat-Free
+
+$f3 -> run();
+
+
+
+
+
