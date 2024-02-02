bf1f77706035992c1917fdf1f37a335284a1adfc
Huy Nguyen <hnguyen10082000@gmail.com>
2024-02-02 01:28:02 -0500

    added routing and sessions for new pages

--- index.php
+++ index.php
@@ -23,6 +23,56 @@ $f3->route('GET /', function () {
     $view = new Template();
     echo $view->render('views/home.html');
 });
+
+// Define a breakfast route
+$f3->route('GET /breakfast', function () {
+    //echo "<h1> Test Test</h1>";
+
+    // Display a view page
+    $view = new Template();
+    echo $view->render('views/breakfast-menu.html');
+});
+
+// Define a lunch route
+$f3->route('GET /lunch', function () {
+    //echo "<h1> Test Test</h1>";
+
+    // Display a view page
+    $view = new Template();
+    echo $view->render('views/lunch-menu.html');
+});
+
+// Define a order route
+$f3->route('GET|POST /order', function ($f3) {
+    //echo "<h1> Test Test</h1>";
+
+    if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+
+        //Validate the data
+        $food = $_POST['food'];
+        $meal = $_POST['meal'];
+        // Put the data in the session array
+        $f3->set('SESSION.food', $food);
+        $f3->set('SESSION.meal', $meal);
+
+        //Redirect to order2
+        $f3->reroute('summary');
+    }
+
+    // Display a view page
+    $view = new Template();
+    echo $view->render('views/order1.html');
+});
+
+// Define a summary route
+$f3->route('GET /summary', function () {
+    //echo "<h1> Test Test</h1>";
+
+    // Display a view page
+    $view = new Template();
+    echo $view->render('views/order-summary.html');
+});
+
 //Run Fat-Free
 
 $f3 -> run();
